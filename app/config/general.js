e1bf04dae7c462101562df811691e01aac219262
Hrvoje <htonkovac@gmail.com>
2018-05-21 00:02:14 +0200

    changed mongo port

--- app/config/general.js
+++ app/config/general.js
@@ -3,5 +3,6 @@ require('dotenv').config()
 module.exports = {
     databaseurl: process.env.MONGO_NETWORK_ALIAS,
     databaseuser: process.env.MONGO_ADMIN_USER,
-    databasepass: process.env.MONGO_ADMIN_PASS
+    databasepass: process.env.MONGO_ADMIN_PASS,
+    databaseport: process.env.MONGO_PORT
 }
